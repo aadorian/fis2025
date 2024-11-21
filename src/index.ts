c38ffe4b2a3b3cd08b27bb7c442d25894fcda96b
usersolidity <usersolidit@gmail.com>
2024-11-21 17:30:49 -0300

    add first commits

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,20 @@
-import '../examples/accordion';
-import '../examples/carousel';
-import '../examples/collapse';
-import '../examples/dial';
-import '../examples/dismiss';
-import '../examples/drawer';
-import '../examples/dropdown';
-import '../examples/popover';
-import '../examples/tabs';
-import '../examples/modal';
-import '../examples/tooltip';
-import '../examples/input-counter';
-import '../examples/clipboard';
-import '../examples/datepicker';
+import { Person } from './domain/Person';
 
-import Person from '../src/domain/person';
-
-
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   const updateButton = document.getElementById(
-    'updateButton',
+    "updateButton",
   ) as HTMLButtonElement;
   if (updateButton) {
-    updateButton.addEventListener('click', updatePerson);
+    updateButton.addEventListener("click", updatePerson);
   }
 });
 
 function updatePerson() {
-  const name = (document.getElementById('name') as HTMLInputElement).value;
-  const age = (document.getElementById('age') as HTMLInputElement)?.value;
-  const root = document.getElementById('root');
+  const name = (document.getElementById("name") as HTMLInputElement).value;
+  const age = (document.getElementById("age") as HTMLInputElement)?.value;
+  const root = document.getElementById("root");
 
-  const person = new Person(name,3);
+  const person = new Person(name, 3);
 
   if (root) {
     root.innerHTML = `<h1>Updated Person: ${person.getName()}</h1>`;
