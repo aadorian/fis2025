86b294b14470e4cb7cf1b594ad4bf06b0a8b68eb
usersolidity <usersolidit@gmail.com>
2024-11-21 19:24:51 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,31 @@ import './tooltip';
 import './input-counter';
 import './clipboard';
 import './datepicker';
+
+import Person from './domain/person';
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const updateButton = document.getElementById(
+    'updateButton',
+  ) as HTMLButtonElement;
+  if (updateButton) {
+    updateButton.addEventListener('click', updatePerson);
+  }
+});
+
+function updatePerson() {
+  const name = (document.getElementById('name') as HTMLInputElement).value;
+  const age = (document.getElementById('age') as HTMLInputElement)?.value;
+  const root = document.getElementById('root');
+
+  const person = new Person(name, age ? parseInt(age) : undefined);
+
+  if (root) {
+    root.innerHTML = `<h1>Updated Person: ${person.getName()}</h1>`;
+    if (age) {
+      root.innerHTML += `<p>Age: ${person.getAge()}</p>`;
+    }
+  }
+}
