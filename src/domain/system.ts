0e40bea3a5cd4219f971b85fd66f06b95bb2d047
usersolidity <usersolidit@gmail.com>
2024-11-21 22:23:18 -0300

    add JSDoc

--- src/domain/system.ts
+++ src/domain/system.ts
@@ -1,4 +1,5 @@
-import Person  from './person';
+
+import Person from './person';
 
 class System {
     private listOfPersons: Person[];
@@ -7,27 +8,42 @@ class System {
         this.listOfPersons = [];
     }
 
-
+    /**
+     * Adds a person to the list if they are not already in the list.
+     * @param {Person} person - The person to add.
+     * @throws {Error} If the person is already in the list.
+     */
     addPerson(person: Person): void {
         if (this.isPersonInList(person)) {
             throw new Error('Person is already in the list.');
         } else {
             this.listOfPersons.push(person);
         }
-        
     }
 
+    /**
+     * Removes a person from the list.
+     * @param {Person} person - The person to remove.
+     */
     removePerson(person: Person): void {
         this.listOfPersons = this.listOfPersons.filter(p => p !== person);
     }
 
-    getPersons(): Person[] {
-        return this.listOfPersons;
-    }
+    /**
+     * Checks if a person is already in the list.
+     * @param {Person} person - The person to check.
+     * @returns {boolean} True if the person is in the list, false otherwise.
+     */
     private isPersonInList(person: Person): boolean {
         return this.listOfPersons.some(p => p === person);
     }
-
+    /**
+     * Returns the list of persons.
+     * @returns {Person[]} The list of persons.
+     *  
+     *  */
+    getPersons(): Person[] {
+        return this.listOfPersons;
+    }
 }
-
 export { System };
